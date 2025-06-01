2b72ecfd36d81c109a64d50f0bb817449b1c7492
mielc <mielcarekkrzysztof0@gmail.com>
2025-06-01 12:02:32 +0200

    Initial Spring Boot project setup with PostgreSQL configuration

---
+++ src/main/java/com/example/vacation_management/VacationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacation_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationManagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/vacation_management/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.vacation_management.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Tymczasowo wyłączamy zabezpieczenia dla ułatwienia developmentu
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
